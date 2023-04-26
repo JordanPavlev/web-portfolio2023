19b770cd39df73eb743ab247717f111533c7df5f
JordanPavlev <danipavlev@gmail.com>
2023-04-26 23:32:23 +0300

    added toggle icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import './styles.scss'
 import logo from './logo-header-removebg-preview.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,8 +9,14 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false)
-  const [click, isClicked] = useState(false)
-  library.add(faBars)   
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setNavbarOpen((prev) => !prev)
+    setIsClicked((click) => !click)
+  }
+  library.add(faBars, faXmark)   
   return (
     <>
       <nav className="navbar header">
@@ -39,9 +45,9 @@ const Navbar = () => {
         </div>
         <button
           className="toggle"
-          onClick={() => setNavbarOpen((prev) => !prev)}
+          onClick={handleClick}
         >
-          <FontAwesomeIcon icon="fa-solid fa-bars" />
+         <i className='hamburger'> {isClicked ?  <FontAwesomeIcon icon="fa-xmark" />: <FontAwesomeIcon icon="fa-solid fa-bars" /> }</i>
         </button>
       </nav>
     </>
